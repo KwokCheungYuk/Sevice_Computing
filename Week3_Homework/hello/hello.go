bb7c7de0063980dea142c627b6265daed7c4962c
KwokCheungYuk <691215689@qq.com>
2019-09-09 15:12:31 +0800

    第三周作业

---
+++ Week3_Homework/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/KwokCheungYuk/Sevice_Computing/Week3_Homework/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH") + "\n")
+}
